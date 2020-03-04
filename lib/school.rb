b8b27f9db647fa09fbb215a84ba01dd8f09ec6e1
Joel Hobgood <jhobgoo@gmail.com>
2020-03-04 23:52:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,10 @@ class School
   end
   
   def add_student(name, grade)
-    if !roster.include?(grade)
-      roster[grade] = []
+    if !roster.include?(grade, name)
+      roster[grade, name] = []
     else 
-      roster[grade] << "name"
+      roster[grade] << 
     end
   end
   
