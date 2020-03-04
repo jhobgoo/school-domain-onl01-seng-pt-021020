cee1d291246c2132ef0c7f4721e04620a3f43491
Joel Hobgood <jhobgoo@gmail.com>
2020-03-04 23:24:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+
+  attr_accessor = :name, :grade
+
+  def initialize(name_of_school)
+    @name = name_of_school
+  end
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student(name, grade)
+    if !roster.include?(grade)
+      roster[grade] = []
+    else roster[grade] << name
+    end
+  end
+  
+end
